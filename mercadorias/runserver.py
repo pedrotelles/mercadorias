db88c38f0c157db50c5143a9c037069300e2d5d3
lorddorn <pedrotelles@gec.inatel.br>
2017-09-26 11:00:55 -0300

    'test'

--- mercadorias/runserver.py
+++ mercadorias/runserver.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 """
 This script runs the mercadorias application using a development server.
 """
@@ -6,6 +8,7 @@ from os import environ
 from mercadorias import app
 
 if __name__ == '__main__':
+    environ['TZ'] = 'America/Sao_Paulo'
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
